4749415fd46144627df82ddb53ceed9766f283ab
Mikhail Kuzin <info@kuzinobit.com>
2024-11-05 16:02:06 +0300

    add type lesson 2

---
+++ lesson_2/type_variable.py
@@ -0,0 +1,22 @@
+# Целые числа (int)
+age = 30
+temperature = -5
+
+# Числа с плавающей точкой (float)
+pi = 3.1416
+weight = 65.5
+
+# Логический тип (bool)
+is_active = True
+has_permission = False
+
+# Строки (str)
+message = "Hello, World!"
+name = 'Иван'
+multi_line_text = """Это многострочный 
+текст"""
+
+# Тип None
+result = None
+
+
