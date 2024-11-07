a13e439091ff879c927562579921a8b31557b9fc
Mikhail Kuzin <info@kuzinobit.com>
2024-11-08 01:12:09 +0300

    add examples lesson 7

---
+++ lesson_7/example_3.py
@@ -0,0 +1,20 @@
+day_number = 3
+
+if day_number == 1:
+    day = "Понедельник"
+elif day_number == 2:
+    day = "Вторник"
+elif day_number == 3:
+    day = "Среда"
+elif day_number == 4:
+    day = "Четверг"
+elif day_number == 5:
+    day = "Пятница"
+elif day_number == 6:
+    day = "Суббота"
+elif day_number == 7:
+    day = "Воскресенье"
+else:
+    day = "Некорректный номер дня"
+
+print("День недели:", day)
