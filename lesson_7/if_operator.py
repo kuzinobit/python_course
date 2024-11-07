215bcfc2460bfb87b77c7f95f6b39ca874746cc8
Mikhail Kuzin <info@kuzinobit.com>
2024-11-08 00:58:08 +0300

    add if_operator lesson 7

---
+++ lesson_7/if_operator.py
@@ -0,0 +1,22 @@
+# Использование IF
+age = 18
+if age >= 18:
+    print("Вы совершеннолетний.")
+print()
+
+# Использование IF-ELSE
+age = 16
+if age >= 18:
+    print("Вы совершеннолетний.")
+else:
+    print("Вы несовершеннолетний.")
+print()
+
+# Использование IF-ELIF-ELSE
+score = 85
+if score >= 90:
+    print("Отлично!")
+elif score >= 70:
+    print("Хорошо.")
+else:
+    print("Нужно постараться.")
