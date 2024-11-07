eec5ec8d7fbe4ba6322f516b513c1330bca76c89
Mikhail Kuzin <info@kuzinobit.com>
2024-11-07 13:54:07 +0300

    add check_content lesson 3

---
+++ lesson_3/check_content.py
@@ -0,0 +1,7 @@
+text = "Python3"
+print(text.startswith("Py"))  # True
+print(text.endswith("3"))     # True
+print(text.isdigit())         # False
+print("12345".isdigit())      # True
+print("Hello".isalpha())      # True
+print("Hello123".isalnum())   # True
