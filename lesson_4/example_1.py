38187524abb4f106c6c598e28b775769e4437d4d
Mikhail Kuzin <info@kuzinobit.com>
2024-11-07 17:18:50 +0300

    add example_1 lesson 4

---
+++ lesson_4/example_1.py
@@ -0,0 +1,6 @@
+length = 10
+width = 5
+perimeter = 2 * (length + width)
+area = length * width
+print("Периметр прямоугольника:", perimeter)  # 30
+print("Площадь прямоугольника:", area)        # 50
