3d2a2f0cfd499b2ae1a63a38802c879a0fc23c36
Mikhail Kuzin <info@kuzinobit.com>
2024-11-08 00:08:31 +0300

    add metod_update_list lesson 6

---
+++ lesson_6/metod_update_list.py
@@ -0,0 +1,9 @@
+# Использование reverse()
+numbers = [1, 2, 3, 4, 5]
+numbers.reverse()
+print(numbers)  # [5, 4, 3, 2, 1]
+
+# Использование sort()
+numbers = [3, 1, 4, 2, 5]
+numbers.sort()
+print(numbers)  # [1, 2, 3, 4, 5]
