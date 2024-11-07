1e787b2d8f7a8115853feb3925be4efe15fb330e
Mikhail Kuzin <info@kuzinobit.com>
2024-11-07 11:51:27 +0300

    add example_1 lesson 2

---
+++ lesson_2/example_1.py
@@ -0,0 +1,19 @@
+# Целое число
+age = 25
+print("Возраст:", age)
+print("Тип данных:", type(age))  # <class 'int'>
+
+# Число с плавающей точкой
+height = 1.75
+print("Рост:", height)
+print("Тип данных:", type(height))  # <class 'float'>
+
+# Строка
+name = "Александр"
+print("Имя:", name)
+print("Тип данных:", type(name))  # <class 'str'>
+
+# Логическое значение
+is_student = True
+print("Студент:", is_student)
+print("Тип данных:", type(is_student))  # <class 'bool'>
