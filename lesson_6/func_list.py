f273fbeaf68acceafc6da3afdd107cd0d1f59301
Mikhail Kuzin <info@kuzinobit.com>
2024-11-08 00:30:53 +0300

    add func_list lesson 6

---
+++ lesson_6/func_list.py
@@ -0,0 +1,27 @@
+# Использование list()
+string = "Python"
+char_list = list(string)
+print(char_list)  # Вывод: ['P', 'y', 't', 'h', 'o', 'n']
+print()
+
+# Использование len()
+numbers = [1, 2, 3, 4, 5]
+print(len(numbers))  # Вывод: 5
+print()
+
+# Использование min() и max()
+print(min(numbers))  # Вывод: 1
+print(max(numbers))  # Вывод: 5
+print()
+
+# Использование sum()
+total = sum(numbers)
+print(total)  # Вывод: 15
+print()
+
+# Использование sorted()
+unsorted_numbers = [3, 1, 4, 2, 5]
+sorted_numbers = sorted(unsorted_numbers)
+print(sorted_numbers)       # Вывод: [1, 2, 3, 4, 5]
+print(unsorted_numbers)     # Вывод: [3, 1, 4, 2, 5]
+
