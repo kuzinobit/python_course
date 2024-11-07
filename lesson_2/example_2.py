3d32b48ca21720eb39eb571604f526cc7e53aee5
Mikhail Kuzin <info@kuzinobit.com>
2024-11-07 11:55:22 +0300

    add example_2 lesson 2

---
+++ lesson_2/example_2.py
@@ -0,0 +1,17 @@
+# Преобразование строки в число
+number_str = "100"
+number_int = int(number_str)
+print("Число:", number_int)
+print("Тип данных:", type(number_int))  # <class 'int'>
+
+# Преобразование числа в строку
+age = 30
+age_str = str(age)
+print("Возраст:", age_str)
+print("Тип данных:", type(age_str))  # <class 'str'>
+
+# Преобразование числа с плавающей точкой в целое число
+pi = 3.1416
+pi_int = int(pi)
+print(pi_int)           # 3
+print(type(pi_int))     # <class 'int'>
