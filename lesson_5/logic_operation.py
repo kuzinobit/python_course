67a46a9b116a53f86fbe816458690a8a53461ed4
Mikhail Kuzin <info@kuzinobit.com>
2024-11-07 18:02:41 +0300

    add logic_operation lesson 5

---
+++ lesson_5/logic_operation.py
@@ -0,0 +1,41 @@
+# Равно
+result_1 = 5 == 5  # True
+result_2 = 5 == 3  # False
+print(result_1)
+print(result_2)
+print()
+
+
+# Не равно
+result_1 = 5 != 3  # True
+result_2 = 5 != 5  # False
+print(result_1)
+print(result_2)
+print()
+
+# Больше
+result_1 = 5 > 3   # True
+result_2 = 3 > 5   # False
+print(result_1)
+print(result_2)
+print()
+
+# Меньше
+result_1 = 3 < 5   # True
+result_2 = 5 < 3   # False
+print(result_1)
+print(result_2)
+print()
+
+# Больше или равно
+result_1 = 5 >= 5  # True
+result_2 = 4 >= 5  # False
+print(result_1)
+print(result_2)
+print()
+
+# Меньше или равно
+result_1 = 5 <= 5  # True
+result_2 = 6 <= 5  # False
+print(result_1)
+print(result_2)
