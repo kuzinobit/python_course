b3ddd80944b4df9ef3af712da02ceb46380ec859
Mikhail Kuzin <info@kuzinobit.com>
2024-11-08 00:00:37 +0300

    add metod_del_list lesson 6

---
+++ lesson_6/metod_del_list.py
@@ -0,0 +1,16 @@
+# Использование remove()
+fruits = ["яблоко", "банан", "вишня"]
+fruits.remove("банан")
+print(fruits)  # ["яблоко", "вишня"]
+print()
+
+# Использование pop()
+fruits = ["яблоко", "банан", "вишня"]
+last_fruit = fruits.pop()
+print(last_fruit)  # "вишня" print(fruits)      # ["яблоко"]
+print()
+
+# Использование clear()
+fruits = ["яблоко", "банан", "вишня"]
+fruits.clear()
+print(fruits)  # []
