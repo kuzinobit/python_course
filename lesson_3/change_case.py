502bf9a2126066adabe996f34367b1ce45492577
Mikhail Kuzin <info@kuzinobit.com>
2024-11-07 13:34:44 +0300

    add change_case lesson 3

---
+++ lesson_3/change_case.py
@@ -0,0 +1,5 @@
+text = "программирование на python"
+print(text.lower())      # программирование на python
+print(text.upper())      # ПРОГРАММИРОВАНИЕ НА PYTHON
+print(text.capitalize()) # Программирование на python
+print(text.title())      # Программирование На Python
