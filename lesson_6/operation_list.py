c1845500ef0a247d1d92e8a19ce26871010ca442
Mikhail Kuzin <info@kuzinobit.com>
2024-11-07 23:50:34 +0300

    add operation_list lesson 6

---
+++ lesson_6/operation_list.py
@@ -0,0 +1,21 @@
+# Длина списка
+fruits = ["яблоко", "банан", "вишня"]
+print(len(fruits))  # 3
+print()
+
+# Проверка наличия элемента
+fruits = ["яблоко", "банан", "вишня"]
+print("яблоко" in fruits)  # True
+print("груша" in fruits)   # False
+print()
+
+# Конкатенация списков
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+combined = list1 + list2
+print(combined)  # [1, 2, 3, 4, 5, 6]
+print()
+
+# Повторение списка
+numbers = [0] * 5
+print(numbers)  # [0, 0, 0, 0, 0]
